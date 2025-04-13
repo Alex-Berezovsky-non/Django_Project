1f9bbb37712953c1fa2b01c3188124a0cf4bc921
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-04-13 22:10:58 +0300

    hw_39: добавлена модель Service с полями name, description, price, duration, is_popular и image.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator,MaxLengthValidator
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    duration = models.PositiveIntegerField(
+        verbose_name="Длительность",
+        help_text="Время выполнения в минутах"
+    )
+    is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
+    image = models.ImageField(upload_to="services/", blank=True, verbose_name="Изображение")
+
+    def __str__(self):          
+        return str(self.name)   
+
+    class Meta:                 
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
