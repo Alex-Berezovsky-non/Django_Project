6a95fcb6bb3d4e8468acc532cd2d1432d0546241
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-05-01 21:08:49 +0300

    hw_42: Добавлены шаблон формы отзыва, URL-маршруты и базовый функционал.  Исправлен URL-путь для страницы благодарности ( Изменен путь с '/thanks' на 'thanks/' для корректной маршрутизации).

--- barbershoptest/urls.py
+++ barbershoptest/urls.py
@@ -8,10 +8,12 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing, name='landing'),
-    path('core/thanks.html', views.thanks, name='thanks'),
+    path('/thanks', views.thanks, name='thanks'),
     path('orders/', login_required(views.orders_list), name='orders_list'),
     path('orders/<int:pk>/', login_required(views.order_detail), name='order_detail'),
     path('services/create/', views.ServiceCreateView.as_view(), name='service_create'),
+    path('review/create/', views.create_review, name='create_review'),
+    path('api/master-info/', views.get_master_info, name='get_master_info'),
 ]
 
 if settings.DEBUG:
