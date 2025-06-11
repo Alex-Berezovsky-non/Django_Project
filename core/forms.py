e12e920c44d4026b5ca8556779431756cbe17f20
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-06-11 21:21:09 +0300

    hw_46_update_forms: "Обновление форм для CBV"

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Review, Master
+from .models import Review, Master, Order, Service
 
 class ReviewForm(forms.ModelForm):
     master = forms.ModelChoiceField(
@@ -27,4 +27,58 @@ class ReviewForm(forms.ModelForm):
             'text': 'Текст отзыва',
             'rating': 'Оценка',
             'photo': 'Фото (необязательно)'
+        }
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['client_name', 'phone', 'master', 'services', 'appointment_date', 'comment']
+        widgets = {
+            'appointment_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'client_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'master': forms.Select(attrs={'class': 'form-select'}),
+            'services': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'client_name': 'Имя клиента',
+            'phone': 'Телефон',
+            'master': 'Мастер',
+            'services': 'Услуги',
+            'appointment_date': 'Дата и время записи',
+            'comment': 'Комментарий'
+        }
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'duration': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'name': 'Название услуги',
+            'description': 'Описание',
+            'price': 'Цена',
+            'duration': 'Длительность (мин)',
+            'is_popular': 'Популярная услуга',
+            'image': 'Изображение'
+        }
+
+class ServiceEasyForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = ['name', 'price']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'name': 'Название услуги',
+            'price': 'Цена',
         }
