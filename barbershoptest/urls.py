b8854e4345e06776c1fee0b66c20a4b67942c87d
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-06-11 21:23:07 +0300

    hw_46_update_urls: "Обновление URL-конфигурации для CBV"

--- barbershoptest/urls.py
+++ barbershoptest/urls.py
@@ -1,21 +1,42 @@
 from django.contrib import admin
-from django.urls import path, include  # Добавьте include
-from django.contrib.auth.decorators import login_required
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core import views
+from django.contrib.auth.decorators import login_required
+from core import views as core_views
 
 urlpatterns = [
+    # Админка
     path('admin/', admin.site.urls),
-    path('', views.landing, name='landing'),
-    path('thanks/', views.thanks, name='thanks'),
-    path('orders/', login_required(views.orders_list), name='orders_list'),
-    path('orders/<int:pk>/', login_required(views.order_detail), name='order_detail'),
-    path('services/create/', views.ServiceCreateView.as_view(), name='service_create'),
-    path('review/create/', views.create_review, name='create_review'),
-    path('api/master-info/', views.get_master_info, name='get_master_info'),
+    
+    # Основные URL приложения core
+    path('', core_views.LandingPageView.as_view(), name='landing'),
+    path('thanks/<str:source>/', core_views.ThanksView.as_view(), name='thanks'),
+    
+    # Защищенные URL (требуют аутентификации и staff-статуса)
+    path('orders/', login_required(core_views.OrdersListView.as_view()), name='orders_list'),
+    path('orders/<int:pk>/', login_required(core_views.OrderDetailView.as_view()), name='order_detail'),
+    path('services/', login_required(core_views.ServicesListView.as_view()), name='services_list'),
+    
+    # URL для мастеров
+    path('master/<int:master_id>/', core_views.MasterDetailView.as_view(), name='master_detail'),
+    
+    # URL для работы с услугами
+    path('services/create/', core_views.ServiceCreateView.as_view(), name='service_create'),
+    path('services/update/<int:pk>/', core_views.ServiceUpdateView.as_view(), name='service_update'),
+    
+    # URL для создания заказа и отзыва
+    path('order/create/', core_views.OrderCreateView.as_view(), name='order_create'),
+    path('review/create/', core_views.ReviewCreateView.as_view(), name='create_review'),
+    
+    # AJAX API
+    path('api/master-services/', core_views.MastersServicesAjaxView.as_view(), name='master_services'),
+    path('api/master-info/', core_views.MasterInfoAjaxView.as_view(), name='get_master_info'),
+    
+    # Подключаем URL из приложения users с пространством имён 'users'
     path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
